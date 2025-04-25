8cfe3b63da7a4df28a5afcc85b3b2c5684c78f50
filcencetti <cencetti.filippo3@gmail.com>
2025-04-25 11:47:58 +0200

    Esercizio svolto, ma soluzione diversa

--- UI/controller.py
+++ UI/controller.py
@@ -13,13 +13,25 @@ class Controller:
         self.fillIDMap()
 
     def handleWorstCase(self, e):
+        self._view._txtOut.controls.clear()
         id = ""
         for nerc in self._model.listNerc:
             if nerc._value == self._view._ddNerc.value:
                 id = nerc.id
                 break
-        self.worst_case = self._model.worstCase(Nerc(id,self._view._ddNerc.value),
-                                                self._view._txtYears.value, self._view._txtHours.value)
+        if self._view._txtYears.value is None or self._view._txtHours.value is None or self._view._txtHours.value == "" or self._view._txtYears.value == "":
+            return self._view.create_alert("Inserisci i valori!")
+
+        self._model.worstCase(Nerc(id,self._view._ddNerc.value),int(self._view._txtYears.value), int(self._view._txtHours.value))
+
+        # TEXT OUTPUT
+        self._view._txtOut.controls.append(ft.Text(f"Tot people affected: {self._model.max_customers}"))
+        self._view._txtOut.controls.append(ft.Text(f"Tot hours of outage: {self._model.sum_hours}"))
+        for event in self._model.best_seq:
+            self._view._txtOut.controls.append(ft.Text(event))
+
+        self._view.update_page()
+
 
     def fillDD(self):
         nercList = self._model.listNerc
--- model/model.py
+++ model/model.py
@@ -1,3 +1,5 @@
+from copy import deepcopy
+
 from database.DAO import DAO
 
 
@@ -7,20 +9,51 @@ class Model:
         self._listNerc = None
         self._listEvents = None
         self.loadNerc()
+        self.best_seq = []
+        self.max_customers = 0
+        self.customers = 0
+        self.sum_hours = 0
 
     def worstCase(self, nerc, maxY, maxH):
-        self._listNerc = self.loadEvents(nerc)
-
-        pass
+        self.loadEvents(nerc)
+        for pos in range(len(self._listEvents)-1):
+            parziale = [self._listEvents[pos]]
+            self.customers = self._listEvents[pos].customers_affected
+            self.ricorsione(parziale,maxY,maxH,pos)
+        return
 
     def ricorsione(self, parziale, maxY, maxH, pos):
+        # esco dalla funzione se arrivo all'ultimo evento della lista
+        if pos == len(self._listEvents) - 1:
+            return
+        # vincolo sugli anni
+        event = self._listEvents[pos + 1]
+        event0 = parziale[0]
+        difference = event.date_event_finished.year - event0.date_event_began.year
+        # vincolo sulle ore
+        sum = 0
+        for ev in parziale:
+            sum += (ev.date_event_finished - ev.date_event_began).total_seconds()
+        sum_with_new_event = (sum + (event.date_event_finished - event.date_event_began).total_seconds()) / 3600
+        # se aggiungendo un evento i vincoli non sono rispettati esco dalla funzione
+        if difference > maxY or sum_with_new_event > maxH:
+            # verifico se la soluzione che ho trovato è ottima
+            if self.max_customers < self.customers:
+                self.max_customers = self.customers
+                self.best_seq = deepcopy(parziale)
+                self.sum_hours= sum / 3600
+            return
 
+        # se l'evento rispetta i vincoli, lo aggiungo, aggiorno il totale e invoco di nuovo la funzione, passando l'indice dell'evento successivo
+        else:
+            parziale.append(event)
+            self.customers += event.customers_affected
+            self.ricorsione(parziale, maxY, maxH, pos+1)
 
-
-        pass
 
     def loadEvents(self, nerc):
-        self._listEvents = DAO.getAllEvents(nerc)
+        self._listEvents = sorted(DAO.getAllEvents(nerc), key=lambda event: event.date_event_began)
+
 
     def loadNerc(self):
         self._listNerc = DAO.getAllNerc()
